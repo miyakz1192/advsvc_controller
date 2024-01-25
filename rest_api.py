42107bd2ddd478a2492d61f6cf5092c75425f426
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2024-01-25 15:16:45 +0000

    support rest_api

--- rest_api.py
+++ rest_api.py
@@ -57,5 +57,7 @@ def get_summaries(year, month, day):
                                year, month, day)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+app.run(host="0.0.0.0", port=8080, debug=True)
+
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=8080, debug=True)

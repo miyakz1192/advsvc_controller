ca8196bd57f10def38c761cfb0a7334313016329
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2024-02-06 14:54:29 +0000

    let advice2summary to use valid timestamp

--- advice2summary.py
+++ advice2summary.py
@@ -23,38 +23,46 @@ from record import *
 import dbinitializer
 import dbquery
 from dbmodels import *
+from datetime import datetime
 
-descstr = "sample"
-parser = argparse.ArgumentParser(description=descstr)
 
-parser.add_argument('year', type=int)
-parser.add_argument('month', type=int)
-parser.add_argument('day', type=int)
 
-args = parser.parse_args()
-
-engine = dbinitializer.create_engine()
-dbquery.init_and_connect(engine)
-
-advice_texts = []
-for r in dbquery.find_by_day(DialogRecord, args.year, args.month, args.day):
-    t2a = None
-    if r.text2advice is not None:
-        t2a = r.text2advice[:30].replace("\n","")
-    print(f"{r.id},{r.timestamp},{t2a}")
-    advice_texts.append(r.text2advice)
-
-session = dbquery.create_session()
-sr = SummaryRecord()
-temp_uuid = sr.uuid
-session.add_all([sr])
-session.commit()
-session.close()
-
-rec = Advice2SummaryRecord(ident=temp_uuid,
-                           advice_texts=advice_texts,
-                           summary_text=None)
-
-Advice2SummaryServiceReqMessaging().connect_and_basic_publish_record(rec)
+def create_advice2summary(year, month, day):
+    engine = dbinitializer.create_engine()
+    dbquery.init_and_connect(engine)
+    
+    advice_texts = []
+    for r in dbquery.find_by_day(DialogRecord, year, month, day):
+        t2a = None
+        if r.text2advice is not None:
+            t2a = r.text2advice[:30].replace("\n","")
+        print(f"{r.id},{r.timestamp},{t2a}")
+        advice_texts.append(r.text2advice)
+    
+    session = dbquery.create_session()
+    sr = SummaryRecord()
+    sr.timestamp = datetime(year=year, month=month, day=day)
+    temp_uuid = sr.uuid
+    session.add_all([sr])
+    session.commit()
+    session.close()
+    
+    rec = Advice2SummaryRecord(ident=temp_uuid,
+                               advice_texts=advice_texts,
+                               summary_text=None)
+    
+    Advice2SummaryServiceReqMessaging().connect_and_basic_publish_record(rec)
 # rec = Advice2SummaryServiceReqMessaging().connect_and_basic_get_record()
 # print(len(rec.advice_texts))
+
+if __name__ == "__main__":
+    descstr = "sample"
+    parser = argparse.ArgumentParser(description=descstr)
+    
+    parser.add_argument('year', type=int)
+    parser.add_argument('month', type=int)
+    parser.add_argument('day', type=int)
+    
+    args = parser.parse_args()
+
+    create_advice2summary(args.year, args.month, args.day)
--- rest_api.py
+++ rest_api.py
@@ -15,6 +15,7 @@ from record import *
 import dbinitializer
 import dbquery
 from dbmodels import *
+from advice2summary import create_advice2summary
 
 app = Flask(__name__)
 
@@ -30,7 +31,8 @@ def get_record_with_ymd(cls, callback, year, month, day):
         res.append(rj)
 
     ret = json.dumps(res, ensure_ascii=False)
-    return make_response(ret)
+    response = make_response(ret)
+    return (res, response)
 
 def callback_DialogRecord(record):
     rj = { "audio2text": record.audio2text,
@@ -45,16 +47,28 @@ def callback_SummaryRecord(record):
 
 @app.route("/dialogs/<int:year>/<int:month>/<int:day>/")
 def get_dialogs(year, month, day):
-    return get_record_with_ymd(DialogRecord,
-                               callback_DialogRecord,
-                               year, month, day)
+    (_, response) = get_record_with_ymd(DialogRecord,
+                                       callback_DialogRecord,
+                                       year, month, day)
+    return response
 
 
 @app.route("/summaries/<int:year>/<int:month>/<int:day>/")
 def get_summaries(year, month, day):
-    return get_record_with_ymd(SummaryRecord,
-                               callback_SummaryRecord,
-                               year, month, day)
+    print("INFO: calling get_summaries")
+    # ちょっとわかりにくい実装で気持ち悪いが、
+    # getの時にSummaryRecordがなければ、作る要求も出してしまう。
+    # TODO: FIXME: PUT/GETでAPIを使い分けた実装にリファクタする。
+    (res, response) = get_record_with_ymd(SummaryRecord,
+                                          callback_SummaryRecord,
+                                          year, month, day)
+    print(f"INFO: get records={len(res)}")
+    print(res)
+    if(len(res) == 0):
+        print(f"INFO: calling create_advice2summary")
+        create_advice2summary(year, month, day)
+    print(f"INFO: end of get_summaries")
+    return response
 
 
 app.run(host="0.0.0.0", port=8080, debug=True)
